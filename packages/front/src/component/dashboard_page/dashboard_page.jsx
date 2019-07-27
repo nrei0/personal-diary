e8fc1cb3517ffbad209855e1cafe01a3e095417b
Andy Tyurin <dev@ateiri.com>
2019-07-27 15:52:34 +0200

    Add basic sign-in UI

--- packages/front/src/component/dashboard_page/dashboard_page.jsx
+++ packages/front/src/component/dashboard_page/dashboard_page.jsx
@@ -1,8 +1,9 @@
 import React, { useContext } from 'react';
-import { AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
+import { AppBar, Toolbar, IconButton, Typography, Button, Container } from '@material-ui/core';
 import { Menu } from '@material-ui/icons';
 
 import { ConfigContext } from '../../global_context';
+import { SignIn } from '../sign_in';
 
 import { authByGoogle, logout } from './dashboard_page.actions';
 import styles from './dashboard_page.scss';
@@ -16,21 +17,26 @@ export const DashboardPage = () => {
   const { googleAuthURL, googleLogoutURL, isLogged } = useContext(ConfigContext);
 
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="menu">
-          <Menu />
-        </IconButton>
-        <Typography variant="h6">Dashboard</Typography>
-        <div className={styles['login-btn']}>
-          <Button
-            color="inherit"
-            onClick={isLogged ? logout(googleLogoutURL) : authByGoogle(googleAuthURL)}
-          >
-            {isLogged ? 'Logout' : 'Login'}
-          </Button>
-        </div>
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <Menu />
+          </IconButton>
+          <Typography variant="h6">Dashboard</Typography>
+          <div className={styles['login-btn']}>
+            <Button
+              color="inherit"
+              onClick={isLogged ? logout(googleLogoutURL) : authByGoogle(googleAuthURL)}
+            >
+              {isLogged ? 'Logout' : 'Login'}
+            </Button>
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Container className={styles.content} fixed>
+        <SignIn />
+      </Container>
+    </>
   );
 };
